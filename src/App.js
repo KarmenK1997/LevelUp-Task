6f67e0265faf4bef3ff2b2ff5a92615fbc669fb4
Karmen Kovacic-Karamatic <karmenkk1997@gmail.com>
2023-05-25 00:01:28 +0100

    Rework form and form submission

--- src/App.js
+++ src/App.js
@@ -8,30 +8,78 @@ function ValidationInfo({ validity, field }) {
 }
 
 function App() {
-  // useEffect(() => {
-  //   const requestOptions = {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({ d: "2030 3 3", cvv: "563", pan: "1234567890123452" })
-  //   };
+  const [validation, setValidation] = useState(null);
+  const [cvv, setCvv] = useState("");
+  const [pan, setPan] = useState("");
+  const [date, setDate] = useState("");
+
+  async function submit(event) {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ date, cvv, pan })
+    };
+
+    try {
+      fetch("http://127.0.0.1:5000/validate", requestOptions);
+      const response = await fetch("http://127.0.0.1:5000/validate", requestOptions);
+
+      const data = await response.json()
+      setValidation(data)
+      console.log(data)
+    }
+    catch (e) {
+      alert("Something went wrong!")
+    }
+
+    event.preventDefault()
 
   //   fetch("http://127.0.0.1:5000/validate", requestOptions)
   //     .then(response => response.json())
   //     .then(data => console.log(data));
+  }
 
-  // }, []);
 
   return (
     <div>
-      <form action="http://127.0.0.1:5000/validate" method="post" target='blank_'>
-        <label for="cvv">cvv:</label>
-        <input type="text" id="cvv" name="cvv"></input>
-        <label for="pan">pan:</label>
-        <input type="text" id="pan" name="pan"></input>
-        <input type="date" id="date" name="date"></input>
-        <input type="submit" value="Submit"></input>
-      </form>
+      <div className='main'>
+        <div className='window'>
+          <form onSubmit={submit}>
+            <div className='input-container'>
+              <label htmlFor="pan">Pan</label>
+              <input
+                onChange={e => setPan(e.target.value)}
+                value={pan}
+                type="text"
+                id="pan"
+                name="pan"
+              />
+              <ValidationInfo validity={validation?.pan_valid} field="pan" />
+            </div>
+            <div className='input-container'>
+              <label htmlFor="cvv">CVV</label>
+              <input
+                onChange={e => setCvv(e.target.value)}
+                value={cvv}
+                type="text"
+                id="cvv"
+                name="cvv"
+              />
+              <ValidationInfo validity={validation?.cvv_valid} field="cvv" />
+            </div>
+            <div className='input-container'><label htmlFor="">Exp</label>
+              <input
+                onChange={e => setDate(e.target.value)}
+                value={date}
+                type="month"
+                id="date"
+                name="date"
+              <ValidationInfo validity={validation?.date_valid} field="date of expiry" /></div>
+            <input className="submit" type="submit" value="Submit"></input>
+          </form>
+        </div>
 
+      </div>
     </div>
   );
 }
